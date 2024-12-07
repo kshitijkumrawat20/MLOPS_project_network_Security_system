3c2d967824b6ebce0ef621a39d0955c88c462186
kshtitijkumrawat20 <kshitijk146@gmail.com>
2024-12-07 18:11:51 +0530

    data validation done

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from src.components.data_ingestion import DataIngestion
+from src.components.data_validation import DataValidation
 from src.exception.exception import NetworkSecurityException
 from src.logging.logger import logging
-from src.entity.config_entity import Data_ingestion_config, TrainingPipelineConfig
+from src.entity.config_entity import Data_ingestion_config, TrainingPipelineConfig, Data_validation_config
 import sys
 
 if __name__ == "__main__":
@@ -11,7 +12,14 @@ if __name__ == "__main__":
         Data_ingestion  = DataIngestion(data_ingestion_config)
         logging.info("Data ingestion started")
         data_ingestion_artifacts = Data_ingestion.initiate_data_ingestion()
-        print(data_ingestion_artifacts)
+        logging.info("Data ingestion completed")
+
         
+        data_validation_config = Data_validation_config(traingning_pipeline_config)
+        Data_validation = DataValidation(data_ingestion_artifacts, data_validation_config)
+        logging.info("Data validation started")
+        data_validation_artifacts = Data_validation.intiate_data_validation()
+        logging.info("Data validation completed")
+        print(data_validation_artifacts)
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -21,5 +21,15 @@ class Data_ingestion_config:
         self.collection_name = training_pipeline.DATA_INGESTION_COLLECTION_NAME
         self.train_test_split_ratio = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATION
         
-         
+class Data_validation_config:
+    def __init__(self,training_pipeline_config: TrainingPipelineConfig):
+        self.data_validation_dir:str = os.path.join(training_pipeline_config.artifact_dir, training_pipeline.DATA_VALIDATION_DIR_NAMR)
+        self.valid_data_dir:str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_VALID_DIR)
+        self.invalid_data_dir:str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_INVALID_DIR)
+        self.valid_train_file_path:str = os.path.join(self.valid_data_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.valid_test_file_path:str = os.path.join(self.valid_data_dir, training_pipeline.TEST_FILE_NAME)
+        self.invalid_train_file_path:str = os.path.join(self.invalid_data_dir, training_pipeline.TRAIN_FILE_NAME)
+        self.invalid_test_file_path:str = os.path.join(self.invalid_data_dir, training_pipeline.TEST_FILE_NAME)
+        self.drift_report_file_path:str = os.path.join(self.data_validation_dir, training_pipeline.DATA_VALIDATION_DRIFT_REPORT_DIR, training_pipeline.DATA_VALIDATION_DRIFT_REPORT_FILE_NAME)
+        
    
